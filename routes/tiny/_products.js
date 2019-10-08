efe97cc11003d84f646c74130ad796517734d23b
Talisson Ferreira <talissonf@gmail.com>
2019-10-08 13:02:14 -0300

    fix: #1

--- routes/tiny/_products.js
+++ routes/tiny/_products.js
@@ -2,11 +2,16 @@ const Tiny = require('./../../lib/tiny/client')
 const EcomSchema = require('./../../lib/schemas/tiny-to-ecomplus')
 const getConfig = require(process.cwd() + '/lib/store-api/get-config')
 const { insertProduct } = require('../../lib/database')
+const logger = require('console-files')
 
 module.exports = appSdk => {
   return (req, res) => {
     const storeId = parseInt(req.get('x-store-id'), 10)
     const body = req.body
+
+    if (!Array.isArray(body) || body === null || !body.length) {
+      return res.status(406).send('Invalid body')
+    }
 
     getConfig({ appSdk, storeId }, true)
 
@@ -14,49 +19,82 @@ module.exports = appSdk => {
         const { access_token } = objConfig
         const tiny = new Tiny(access_token)
 
-        tiny.fetchProducts(body[0])
+        let current = 0
+        const sync = (skus) => {
+          const sku = skus[current]
+          if (sku) {
+            tiny.findProduct(sku)
+              .then(async result => {
+                let { retorno } = result.data
+                let { status, status_processamento, produtos, codigo_erro } = retorno
+                let { produto } = produtos[0]
 
-          .then(async result => {
-            const { retorno } = result.data
-            const { status_processamento, produto } = retorno
+                if (parseInt(status_processamento) === 3) {
+                  return tiny.fetchProducts(produto.id)
 
-            if (parseInt(status_processamento) === 3) {
-              let schema
-              try {
-                schema = await EcomSchema(produto, tiny)
-              } catch (error) {
-                res.status(400).send(error.message)
-              }
-              console.log(JSON.stringify(schema))
-              const resource = '/products.json'
-              const method = 'POST'
-              return appSdk.apiRequest(storeId, resource, method, schema)
+                    .then(async result => {
+                      let { retorno } = result.data
+                      let { status, status_processamento, produto, codigo_erro } = retorno
 
-                .then((resp) => {
-                  const { _id } = resp.response.data
-                  // insere produto no banco
-                  insertProduct(schema.sku, storeId, _id, body[0], schema.name, schema.price, schema.quantity, 'tiny')
-                  // variação?
-                  if (produto.classe_produto === 'V') {
-                    const setVariations = require('../../lib/variations-to-ecom')(tiny, appSdk, storeId)
-                    setVariations(produto.variacoes, _id, schema.sku) // Ecomplus
-                    // insere variação no banco
+                      if (parseInt(status_processamento) === 3) {
+                        let schema
+                        try {
+                          schema = await EcomSchema(produto, tiny)
+                        } catch (error) {
+                          res.status(400).send(error.message)
+                        }
+
+                        const resource = '/products.json'
+                        const method = 'POST'
+                        return appSdk.apiRequest(storeId, resource, method, schema)
+
+                          .then((resp) => {
+                            const { _id } = resp.response.data
+                            // insere produto no banco
+                            insertProduct(schema.sku, storeId, _id, body[0], schema.name, schema.price, schema.quantity, 'tiny')
+                            // variação?
+                            if (produto.classe_produto === 'V') {
+                              const setVariations = require('../../lib/variations-to-ecom')(tiny, appSdk, storeId)
+                              setVariations(produto.variacoes, _id, schema.sku) // Ecomplus
+                              // insere variação no banco
+                            }
+                            current++
+                            sync(skus)
+                          })
+                      } else {
+                        if (status === 'Erro') {
+                          if (!isNaN(parseInt(codigo_erro)) && parseInt(codigo_erro) === 6) {
+                            // to many requests
+                            console.log('--> API limit reached, sending the product %s in 1m', sku)
+                            setTimeout(() => {
+                              // try again
+                              sync(body)
+                            }, 60000)
+                          } else {
+                            console.log('Product %i not found with this sku', sku)
+                          }
+                        }
+                      }
+                    })
+                } else {
+                  if (!isNaN(parseInt(codigo_erro)) && parseInt(codigo_erro) === 6) {
+                    // to many requests
+                    console.log('--> API limit reached, sending the product %s in 1m', sku)
+                    setTimeout(() => {
+                      // try again
+                      sync(body)
+                    }, 60000)
+                  } else {
+                    console.log('Product %i not found with this sku', sku)
                   }
-                  res.send(resp.response.data)
-                })
-
-                .catch(e => console.log(JSON.stringify(e.response.data)))
-            } else {
-              res.status(404).send({
-                'status': 404,
-                'message': 'Not found',
-                'user_message': {
-                  'en_us': 'No results were found for the requested resource and ID',
-                  'pt_br': 'Nenhum resultado foi encontrado para o recurso e ID solicitado'
                 }
               })
-            }
-          })
+              .catch(e => logger.log('TINY_SYNC_PRODUCTS_ERR', e))
+          }
+        }
+
+        sync(body)
+        res.status(200).end()
       })
   }
 }
